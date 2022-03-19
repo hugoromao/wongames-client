e3f54a547c047e0bac0bf7c5c3dd5910d64c5a20
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-19 10:15:51 -0400

    test Logo color

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,7 +6,14 @@ export type LogoProps = {
 
 const Logo = ({ color = 'white' }: LogoProps) => (
   <S.Wrapper color={color}>
-    <svg width="158" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="158"
+      height="48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-label="Won Games"
+    >
       <path
         d="M.055 15.227.019 35.872c-.008 4.796 5.336 7.663 9.327 5.004l20.596-13.722L50.49 40.947c3.982 2.672 9.335-.176 9.344-4.972l.034-19.997c.014-7.882-7.549-13.565-15.116-11.361l-.873.254a50.875 50.875 0 0 1-29.358-.27C7.351 2.366.068 7.716.055 15.226Z"
         fill="url(#a)"
