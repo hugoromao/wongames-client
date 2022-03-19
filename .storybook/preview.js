b6646b59135f821fb20c073e78d954ec4c5f60b8
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-19 09:30:51 -0400

    adding absolute paths on storybook preview file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from '../src/styles/global'
-import theme from '../src/styles/theme'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
   nextRouter: {
