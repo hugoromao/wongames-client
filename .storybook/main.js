01bdf09442f1f7c5773e57746f16c4fda3d7bb0f
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-19 12:01:09 -0400

    MediaMatch component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   staticDirs: ['../public'],
-  stories: ['../src/components/**/stories.tsx'],
+  stories: [
+    '../src/components/**/*.stories.tsx',
+    '../src/components/**/stories.tsx'
+  ],
   addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
   core: {
     builder: 'webpack5'
