d174caf908881a26f8e68793b50afbd886ab9c65
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-19 09:39:28 -0400

    creating Logo component

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Logo from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo
+} as Meta
+
+export const Default: Story = () => <Logo />
