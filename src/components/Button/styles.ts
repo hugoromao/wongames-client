d3292287a571ec0b1967ecc4f6183f1463936476
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-21 14:31:54 -0400

    component Button and tests

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,38 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import { ButtonProps } from '.'
+
+type WrapperProps = Pick<ButtonProps, 'size'>
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(
+      178.59deg,
+      #ff5f5f -14.51%,
+      #f062c0 102.86%,
+      #f23131 102.86%
+    );
+    color: ${theme.colors.white};
+    border: none;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+
+    ${!!size && wrapperModifiers[size](theme)}
+  `}
+`
