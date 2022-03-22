0ef6982498e401c3a0a01a7725cd2a63ba1c4e69
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-22 14:40:38 -0400

    init component Banner

---
+++ src/components/Banner/Banner.test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(
+      <Banner
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+  })
+})
