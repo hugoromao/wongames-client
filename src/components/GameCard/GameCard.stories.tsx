1660553f158ea943eff95c7a90897a3e51859c8b
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-23 17:18:51 -0400

    GameCardSlider component

--- src/components/GameCard/GameCard.stories.tsx
+++ src/components/GameCard/GameCard.stories.tsx
@@ -1,4 +1,5 @@
-import { Story, Meta } from '@storybook/react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
 import GameCard, { GameCardProps } from '.'
 
 export default {
@@ -9,22 +10,22 @@ export default {
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 200,00'
+    promotionalPrice: 'R$ 215,00'
   },
   argTypes: {
-    onFav: { action: 'Clicked' },
+    onFav: { action: 'clicked' },
     ribbon: { type: 'string' }
   }
 } as Meta
 
 export const Default: Story<GameCardProps> = (args) => (
-  <div style={{ width: '30rem', margin: '0 auto' }}>
+  <div style={{ width: '30rem' }}>
     <GameCard {...args} />
   </div>
 )
 
 export const WithRibbon: Story<GameCardProps> = (args) => (
-  <div style={{ width: '30rem', margin: '0 auto' }}>
+  <div style={{ width: '30rem' }}>
     <GameCard {...args} />
   </div>
 )
