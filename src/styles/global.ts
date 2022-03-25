18cfe677b79c0c6403b830622503db3d7c7d097d
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-25 14:55:43 -0400

    adding mainBg on global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -32,6 +32,11 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
   ${({ theme }) => css`
@@ -42,6 +47,7 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      background-color: ${theme.colors.mainBg};
     }
   `}  
 `
