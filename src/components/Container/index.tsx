ea52f7c956e889de2243c0c1a68dccaef9045a85
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-25 14:52:39 -0400

    creating Container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components'
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.grid.container};
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    padding-left: calc(${theme.grid.gutter} / 2);
+    padding-right: calc(${theme.grid.gutter} / 2);
+  `}
+`
