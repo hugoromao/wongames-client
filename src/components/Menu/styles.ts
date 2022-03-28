61e2126854116e93a0e5041852501dd7e55b88e9
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-28 17:50:04 -0400

    init Checkbox component styles

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -99,7 +99,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     right: 0;
     height: 100vh;
     overflow: hidden;
-    transition: opacity 0.3s ease-in-out;
+    transition: opacity 0.3s ${theme.transition.fast};
     opacity: ${isOpen ? 1 : 0};
     pointer-events: ${isOpen ? 'all' : 'none'};
 
@@ -127,12 +127,12 @@ export const MenuFull = styled.nav<MenuFullProps>`
       font-size: ${theme.font.sizes.xlarge};
       margin-bottom: ${theme.spacings.small};
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform 0.3s ${theme.transition.fast};
     }
 
     ${RegisterBox} {
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform 0.3s ${theme.transition.fast};
     }
   `}
 `
