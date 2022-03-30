f480c869ee42cfeac99095fdc87fd17398619adb
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-30 16:12:45 -0400

    GameInfo component

---
+++ src/components/GameInfo/GameInfo.stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react'
+import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'GameInfo',
+  component: GameInfo,
+  args: mockGame,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: '144rem', padding: '1.5rem' }}>
+    <GameInfo {...args} />
+  </div>
+)
