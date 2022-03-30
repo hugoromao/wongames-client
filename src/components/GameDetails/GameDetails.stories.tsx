63e63da92440e5e95ab5d6434ec3ba6adf53fa16
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-30 17:07:55 -0400

    GameDetails component

---
+++ src/components/GameDetails/GameDetails.stories.tsx
@@ -0,0 +1,39 @@
+import { Story, Meta } from '@storybook/react'
+import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame,
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    },
+    releaseDate: {
+      control: {
+        type: 'date'
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Action', 'Survival']
+      }
+    }
+  }
+} as Meta
+
+export const Default: Story<GameDetailsProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameDetails {...args} />
+  </div>
+)
---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,87 @@
+import { Apple, Windows, Linux } from '@styled-icons/fa-brands'
+
+import Heading from 'components/Heading'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+
+type Platform = 'windows' | 'linux' | 'mac'
+
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
+
+export type GameDetailsProps = {
+  developer: string
+  platforms: Platform[]
+  releaseDate: string
+  rating: Rating
+  genres: string[]
+}
+
+const GameDetails = ({
+  developer,
+  platforms,
+  releaseDate,
+  rating,
+  genres
+}: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux title="Linux" size={18} />,
+    mac: <Apple title="Mac" size={18} />,
+    windows: <Windows title="Windows" size={18} />
+  }
+
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
+
+      <S.Content>
+        <S.Block>
+          <S.Label>{developer}</S.Label>
+          <S.Description>Gearbox Software</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Release Date</S.Label>
+          <S.Description>
+            {new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(releaseDate))}
+          </S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Platforms</S.Label>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Publisher</S.Label>
+          <S.Description>2K</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>
+            {rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`}
+          </S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Genres</S.Label>
+          <S.Description>{genres.join(' / ')}</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default GameDetails
