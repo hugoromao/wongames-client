dfe887a802461a373e26c2c22463a4f2120ef2b0
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-31 15:01:33 -0400

    Gallery component

---
+++ src/components/Gallery/Gallery.test.tsx
@@ -0,0 +1,76 @@
+import 'match-media-mock'
+import { fireEvent, screen } from '@testing-library/react'
+
+import Gallery from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import mockItems from './mock'
+
+describe('<Gallery />', () => {
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', mockItems[0].src)
+  })
+
+  it('should handle open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+  })
+
+  it('should handle open modal with selected image', async () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    )
+
+    const img = await screen.findByRole('img', { name: /Gallery Image 2/i })
+    expect(img.parentElement?.parentElement).toHaveClass('slick-active')
+  })
+
+  it('should handle close modal when overlay or button clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should handle close modal when ESC button is pressed', () => {
+    const { container } = renderWithTheme(
+      <Gallery items={mockItems.slice(0, 2)} />
+    )
+
+    const modal = screen.getByLabelText('modal')
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    fireEvent.keyUp(container, { key: 'Escape' })
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
+})
---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState, useRef } from 'react'
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+import SlickSlider from 'react-slick'
+
+import Slider, { SliderSettings } from 'components/Slider'
+
+import * as S from './styles'
+import { Close } from '@styled-icons/material-outlined'
+
+const commonSetting: SliderSettings = {
+  infinite: false,
+  lazyLoad: 'ondemand',
+  arrows: true,
+  nextArrow: <ArrowRight aria-label="next image" />,
+  prevArrow: <ArrowLeft aria-label="previous image" />,
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    }
+  ]
+}
+
+const settings: SliderSettings = {
+  ...commonSetting,
+  slidesToShow: 4
+}
+
+const modalSettings: SliderSettings = {
+  ...commonSetting,
+  slidesToShow: 1
+}
+
+export type GalleryImageProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleryImageProps[]
+}
+
+const Gallery = ({ items }: GalleryProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const slider = useRef<SlickSlider>(null)
+
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [])
+
+  return (
+    <S.Wrapper>
+      <Slider ref={slider} settings={settings}>
+        {items?.map((item, index) => (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            key={item.src}
+            role="button"
+            src={item.src}
+            alt={`Thumb - ${item.label}`}
+            onClick={() => {
+              setIsOpen(true)
+              slider.current!.slickGoTo(index, true)
+            }}
+          />
+        ))}
+      </Slider>
+      <S.Modal isOpen={isOpen} aria-label="modal" aria-hidden={!isOpen}>
+        <S.Close
+          role="button"
+          aria-label="close modal"
+          onClick={() => setIsOpen(false)}
+        >
+          <Close size={40} />
+        </S.Close>
+
+        <S.Content>
+          <Slider ref={slider} settings={modalSettings}>
+            {items?.map((item) => (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                key={item.src}
+                src={item.src}
+                alt={`gallery - ${item.label}`}
+              />
+            ))}
+          </Slider>
+        </S.Content>
+      </S.Modal>
+    </S.Wrapper>
+  )
+}
+
+export default Gallery
