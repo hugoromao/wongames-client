6b8d82e43bc9306ab681f374d2f1b0e0b9c08f6f
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-18 19:30:29 -0400

    creating CardButton

---
+++ src/components/CartButton/test.tsx
@@ -0,0 +1,46 @@
+import userEvent from '@testing-library/user-event'
+import { CartContextDefaultValues } from 'hooks/use-cart'
+import { render, screen } from 'utils/test-utils'
+import CardButton from '.'
+
+describe('<CartButton />', () => {
+  it('should render button to add and call the method if clicked', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => false,
+      addToCart: jest.fn()
+    }
+
+    render(<CardButton id="1" />, {
+      cartProviderProps
+    })
+
+    const button = screen.getByLabelText(/add to cart/i)
+
+    expect(button).toBeInTheDocument()
+
+    userEvent.click(button)
+
+    expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1')
+  })
+
+  it('should render button to remove and call the method if clicked', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn()
+    }
+
+    render(<CardButton id="1" />, {
+      cartProviderProps
+    })
+
+    const button = screen.getByLabelText(/remove from cart/i)
+
+    expect(button).toBeInTheDocument()
+
+    userEvent.click(button)
+
+    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
+  })
+})
