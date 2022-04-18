e28b390eb1b90d33a98dec2b27cf2713c7b7178b
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-18 14:59:30 -0400

    setting apollo fetch-policy: no-cache

--- src/utils/format-price.ts
+++ src/utils/format-price.ts
@@ -1,4 +1,6 @@
 export default function formatPrice(price: number | bigint): string {
+  if (price === 0) return 'FREE'
+
   return new Intl.NumberFormat('en', {
     style: 'currency',
     currency: 'USD'
