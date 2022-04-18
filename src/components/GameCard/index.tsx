bac4128ee6d7ab66cfca5410d554a236ad2bbce5
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-18 19:34:39 -0400

    adding context to GameCard component

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,17 +1,14 @@
 import Link from 'next/link'
 
-import {
-  AddShoppingCart,
-  Favorite,
-  FavoriteBorder
-} from '@styled-icons/material-outlined'
+import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined'
 
 import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
-import Button from 'components/Button'
 import * as S from './styles'
 import formatPrice from 'utils/format-price'
+import CartButton from 'components/CartButton'
 
 export type GameCardProps = {
+  id: string
   slug: string
   title: string
   developer: string
@@ -26,6 +23,7 @@ export type GameCardProps = {
 }
 
 const GameCard = ({
+  id,
   slug,
   title,
   developer,
@@ -68,7 +66,8 @@ const GameCard = ({
           <S.Price isPromotional>{formatPrice(price)}</S.Price>
         )}
         <S.Price>{formatPrice(promotionalPrice || price)}</S.Price>
-        <Button icon={<AddShoppingCart />} size="small" />
+
+        <CartButton id={id} />
       </S.BuyBox>
     </S.Content>
   </S.Wrapper>
--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -5,6 +5,7 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import GameCard from '.'
 
 const props = {
+  id: '1',
   slug: 'population-zero',
   title: 'Population Zero',
   developer: 'Rockstar Games',
