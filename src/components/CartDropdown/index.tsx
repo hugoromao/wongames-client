eb68130983ad503f2637e3815d7de90ba2bd3a12
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-18 19:18:19 -0400

    adding Context on CartDropdown

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,16 +1,10 @@
 import CartIcon from 'components/CartIcon'
 import CartList from 'components/CartList'
 import Dropdown from 'components/Dropdown'
-import { GameItemProps } from 'components/GameItem'
 
 import * as S from './styles'
 
-export type CartDropdownProps = {
-  items?: GameItemProps[]
-  total?: string
-}
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
+const CartDropdown = () => (
   <S.Wrapper>
     <Dropdown title={<CartIcon />}>
       <CartList hasButton />
--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,21 +1,28 @@
 import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import { render } from 'utils/test-utils'
 
 import items from 'components/CartList/mock'
 
 import CartDropdown from '.'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartDropdown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    renderWithTheme(<CartDropdown items={items} total="R$ 300,00" />)
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: '$300.0'
+    }
+    render(<CartDropdown />, { cartProviderProps })
+  })
 
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
-    renderWithTheme(<CartDropdown items={items} total="R$ 300,00" />)
-
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
