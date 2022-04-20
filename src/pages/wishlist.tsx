4314dfd9d11e761b3db2fdc993257f4741ea8b11
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-19 20:31:13 -0400

    adding protected pages route

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -6,12 +6,16 @@ import { gamesMapper, highlightMapper } from 'utils/mappers'
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 
 import gamesMock from 'components/GameCardSlider/mock'
+import protectedRoutes from 'utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+
   const apolloClient = initializeApollo()
 
   const { data } = await apolloClient.query<QueryRecommended>({
@@ -25,7 +29,8 @@ export async function getStaticProps() {
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
         data.recommended?.section?.highlight
-      )
+      ),
+      session
     }
   }
 }
