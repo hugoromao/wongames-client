165d18dfb3875b3d481b31f7cd0b75cdf874bdbc
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-27 16:31:35 -0400

    testing game page content and add to cart

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,8 +1,11 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Game Page', () => {
-  it('should render game page sections', () => {
+  before(() => {
     cy.visit('/game/spiritfarer-demo')
+  })
+
+  it('should render game page sections', () => {
     cy.wait(10000)
 
     cy.getByDataCy('game-info').within(() => {
@@ -19,5 +22,50 @@ describe('Game Page', () => {
       'have.length.gt',
       0
     )
+
+    // content
+    cy.getByDataCy('content').within(() => {
+      cy.findByRole('heading', { name: /description/i }).should('exist')
+    })
+
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+    // Details
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publisher/i }).should('exist')
+      cy.findByRole('heading', { name: /rating/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+      cy.findAllByText(/Thunder Lotus Games/i).should('have.length', 2)
+      cy.findByText(/Aug 16, 2020/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Adventure / Simulation / Platformer').should('exist')
+    })
+
+    cy.shouldRenderShowcase({
+      name: 'You may like these games',
+      highlight: false
+    })
+  })
+
+  it('should add/remove game in cart', () => {
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i)
+      .first()
+      .should('have.text', 1)
+      .click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', { name: /Spiritfarer Demo/i }).should('exist')
+    })
   })
 })
