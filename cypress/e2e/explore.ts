7d02100657eada82c1374a7e440eb060607123f0
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-27 16:56:42 -0400

    testing show more button

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -8,9 +8,11 @@ import {
 } from '../../src/utils/filter/fields'
 
 describe('Explore page', () => {
-  it('should render filters columns', () => {
+  before(() => {
     cy.visit('/games')
+  })
 
+  it('should render filters columns', () => {
     cy.findByRole('heading', { name: /sort by price/i }).should('exist')
     cy.findByRole('heading', { name: /^price/i }).should('exist')
     cy.findByRole('heading', { name: /platforms/i }).should('exist')
@@ -20,5 +22,11 @@ describe('Explore page', () => {
     cy.getFields(priceFields)
     cy.getFields(platformFields)
     cy.getFields(genreFields)
+  })
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length', 15)
+    cy.findByRole('button', { name: /show more/i }).click()
+    cy.getByDataCy('game-card').should('have.length', 30)
   })
 })
